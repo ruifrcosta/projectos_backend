503398aa57889953a5e2771e9975d50715ceb308
ruifrcosta <152488282+ruifrcosta@users.noreply.github.com>
2024-03-02 18:50:32 +0100

    hoje vou barter um projecto de gerenciamento de notas vou fazer o levantamento de requisitos e vou dar inicio ao projecto

--- basededados.php/insercao_de_dados_na_tabela.php
+++ basededados.php/insercao_de_dados_na_tabela.php
@@ -1,4 +1,6 @@
 <?php
 $sql = "INSERT INTO usuarios (nome, email, password)
-VALUES ('Marta Costa','marta@gmail.com', 'mmmmsmsmdmwdfiwhd92wdgh9wg')";
+VALUES ('Marta Costa','marta@gmail.com', 'mmmmsmsmdmwdfiwhd92wdgh9wg'),
+        ('Rui Costa', 'rui.costa@gmail.com', 'adroidangola0'),
+        ('Anderson Rodrigues', 'matt@gmail.com', 'allow99937838')";
 ?>
